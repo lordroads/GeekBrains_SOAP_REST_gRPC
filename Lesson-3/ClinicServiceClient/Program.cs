328448eb228bc7e1f2c22ceaf982cd5a54cd28d1
Yurii Mitrohin <lordroads@mail.ru>
2023-04-09 16:12:59 +0300

    Решение домашнего задания. Урок 3

---
+++ Lesson-3/ClinicServiceClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicServiceClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
