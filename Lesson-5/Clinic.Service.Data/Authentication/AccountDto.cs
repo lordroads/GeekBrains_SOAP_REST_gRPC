ebad0a525c7c21fd55f3bbd6e5bdd1f85e5e7718
Yurii Mitrohin <lordroads@mail.ru>
2023-04-30 19:29:13 +0300

    Решение домошнего задания. Урок 5

---
+++ Lesson-5/Clinic.Service.Data/Authentication/AccountDto.cs
@@ -0,0 +1,32 @@
+﻿namespace Clinic.Service.Data.Authentication;
+
+/// <summary>
+/// Account entity
+/// </summary>
+public class AccountDto
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public int AccountId { get; set; }
+    /// <summary>
+    /// Email
+    /// </summary>
+    public string EMail { get; set; }
+    /// <summary>
+    /// Locked
+    /// </summary>
+    public bool Locked { get; set; }
+    /// <summary>
+    /// FirstName
+    /// </summary>
+    public string FirstName { get; set; }
+    /// <summary>
+    /// LastName
+    /// </summary>
+    public string LastName { get; set; }
+    /// <summary>
+    /// SecondName
+    /// </summary>
+    public string SecondName { get; set; }
+}
---
+++ Lesson-5/Clinic.Service.Data/Authentication/SessionContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Clinic.Service.Data.Authentication;
+
+public class SessionContext
+{
+    public int SessionId { get; set; }
+
+    public string SessionToken { get; set; }
+
+    public AccountDto Account { get; set; }
+
+
+}
